2c9e013d9d042321a3df78dde8ed25f13be54d82
Stephen Becker IV <github@deathbyescalator.com>
2016-06-01 13:47:23 -0700

    remove hard coded path

--- test.rb
+++ test.rb
@@ -2,7 +2,7 @@ require 'rubygems'
 require 'bundler/setup'
 Bundler.require
 require 'fiddle'
-library = Fiddle.dlopen('/Users/becker/trash/hash_color/target/release/libhash_color.dylib')
+library = Fiddle.dlopen(File.join(File.dirname(__FILE__),'target','release','libhash_color.dylib'))
 
 
 Fiddle::Function.new(library['initialize_rust_color'], [], Fiddle::TYPE_VOIDP).call
